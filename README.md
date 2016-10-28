1df3463a9a3adead6c06e4361f1bf91babbc91ac
Sébastien Castiel <sebastien.castiel@energiency.com>
2016-10-28 20:27:29 +0200

    blog initialization & first post

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the source repository of my blog available at [blog.castiel.me](https://blog.castiel.me).
+
+All content is under [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/) license.
---
+++ miblog.conf.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var process = require('process');
+module.exports = {
+    baseUrl: process.env.BASE_URL || 'http://blog.castiel.me',
+    title: 'Another webdev blog',
+    description: 'I\'m [Sébastien Castiel](https://twitter.com/scastiel), and I talk about web development and some other stuff.',
+    footer: 'All content distributed under [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/) license. Powered by [Miblog](https://github.com/scastiel/miblog).',
+    nbPostsPerPage: 3,
+    postsDirectory: path.join(__dirname, 'posts'),
+    publicDirectory: path.join(__dirname, 'public'),
+    // disqusId: 'blog-castiel-me',
+    // analyticsId: 'UA-86125196-1'
+};
