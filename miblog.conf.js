d34ed7e3eba61d2172c3970b1255e9c41c807269
Sébastien Castiel <sebastien@castiel.me>
2017-05-17 22:46:16 +0200

    new post: writing modern JavaScript code

--- miblog.conf.js
+++ miblog.conf.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Another webdev blog',
     description: 'I\'m [Sébastien Castiel](https://twitter.com/scastiel), and I talk about web development and some other stuff.',
     footer: 'All content distributed under [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/) license. Powered by [Miblog](https://github.com/scastiel/miblog).',
-    nbPostsPerPage: 3,
+    nbPostsPerPage: 10,
     postsDirectory: path.join(__dirname, 'posts'),
     publicDirectory: path.join(__dirname, 'public'),
     disqusId: 'blog-castiel-me',
